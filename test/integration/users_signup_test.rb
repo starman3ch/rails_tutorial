dbfd7949ac2e6c88bbdd0bd3716b05e0c070ae17
changhee <alice616@naver.com>
2015-06-20 16:04:49 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,6 +14,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     						password_confirmation: "bar" }
     	end
     	assert_template 'users/new'
+     assert_select 'div#<CSS id for error explanation>'
+     assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do
@@ -25,6 +27,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                               password_confirmation: "password" }
       end
       assert_template 'users/show'
+      # assert_not flash.FILL_IN
       assert is_logged_in?
     end
 
